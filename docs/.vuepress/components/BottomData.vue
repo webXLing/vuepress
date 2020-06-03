34c331e02ce2c9d9ef47cf3f8e6b5a96515c3084
web_xl <597146220@qq.com>
2020-06-03 10:02:56 +0800

    mysql learn

--- docs/.vuepress/components/BottomData.vue
+++ docs/.vuepress/components/BottomData.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="FirstBottomPage">
-        <a href="http://beian.gov.cn/portal/index.do">粤ICP备19140452号-1</a> | Copyright © 2019-present 裂泉
-    </div>
+  <div class="FirstBottomPage">
+    <a href="http://beian.gov.cn/portal/index.do">粤ICP备19140452号-1</a> | Copyright © 2019-present Love Life
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
 <style>
-.FirstBottomPage{
-    border-top: 1px solid #eaecef;
-    padding-top: 30px;
-    margin-top: 40px;
-    text-align: center;
+.FirstBottomPage {
+  border-top: 1px solid #eaecef;
+  padding-top: 30px;
+  margin-top: 40px;
+  text-align: center;
 }
 </style>
--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,10 @@
 ---
 home: true
 heroImage: /egg.png
-heroText: 裂泉的前端记录
+heroText: Love Life的前端记录
 tagline: 一点一滴都是进步
 actionText: 马上进入 →
-actionLink: /pages/folder1/test1.md
+actionLink: /pages/folder1/p_19_11_25.md
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
@@ -13,7 +13,7 @@ features:
 - title: 高性能
   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
 ---
-<!-- 
+
 <ClientOnly>
   <BottomData/>
-</ClientOnly> -->
+</ClientOnly>
